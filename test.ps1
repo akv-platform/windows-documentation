03b99ed42e5514ac9a10bb2380392b481ce47a1a
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-22 13:30:24 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,10 +1,4 @@
 Write-Host "running test.ps1..."
 
-$imageAlias = $env:ImageAlias
-Write-Host "Image Alias: $imageAlias"
-
-$github = $env:Github
-Write-Host "Github: $github"
-
-$test = $env:Test
-Write-Host "Test: $test"
+$githubFeedPAT = $env:GithubFeedPAT
+Write-Host "Github: $githubFeedPAT"
